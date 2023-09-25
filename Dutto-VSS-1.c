a770d6b021cdcdf72147363adfebf2a63cb86011
Patrick.Dutto <patrick.dutto@itiscuneo.eu>
2023-09-25 08:59:01 +0200

    esercizio 1 e 2
    esercizio 1 e 2 con file da qui prendere i dati

---
+++ Dutto-VSS-1.c
@@ -0,0 +1,76 @@
+/*
+author: Patrick Dutto
+date:18/09/2023
+es.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#define DIM_RIGA 200
+#define NUM_RIGHE 20000
+
+//strtok funzione
+
+typedef struct Film {
+    int numero;
+    char* titoloFilm;
+    char* genere;
+    int annoUscita;
+    char* disponibilita;
+} Film;
+
+
+void stampaFile(Film c[], int n) {
+    int k;
+    for(k=0; k<n; k++) {
+        printf("%d, %c, %c, %d, %c\n",c[k].numero, c[k].titoloFilm, c[k].genere, c[k].annoUscita, c[k].disponibilita);
+
+    }
+
+}
+
+
+int main() {
+
+    char riga[DIM_RIGA];
+    char* campo;
+    Film array_film[NUM_RIGHE];
+    int counter=0;
+
+    FILE *fp = fopen("Ciak_film.txt","r");
+    if(fp==NULL) {
+        printf("file non esiste");
+        exit(1);
+    } else {
+
+        while(fgets(riga, DIM_RIGA, fp)){
+
+            campo=strtok(riga, ",");
+            array_film[counter].numero = atoi(campo);
+
+            campo=strtok(NULL, ",");
+            array_film[counter].titoloFilm = strdup(campo); //strdup ti ritorna la copia
+
+            campo=strtok(NULL, ",");
+            array_film[counter].genere = strdup(campo);
+
+            campo=strtok(NULL, ",");
+            array_film[counter].annoUscita = atoi(campo);
+
+            campo=strtok(NULL, ",");
+            array_film[counter].disponibilita = strdup(campo);
+
+            counter = counter + 1;
+
+        }
+
+        stampaFile(array_film, counter);
+
+        fclose(fp);
+    }
+    return 0;
+}
